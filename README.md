dde5ba33115019bf92c8f0a518ad8df542ca9f79
IvanPagani <ivanpagani07@gmail.com>
2025-04-12 17:31:31 -0300

    Updated README.md and added files for the Linked List project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Linked_Lists
+
+LINKED LISTS<br/>
+Project from <a href="https://www.theodinproject.com/" target="_blank" rel="noopener noreferrer">The Odin Project</a> Foundations Path.<br/>
+<br/>
+First contact with this common type of Data Structure.<br/>
+<br/>
+This project was made using:<br/>
+- RUBY.<br/>
---
+++ lib/linkedlist.rb
@@ -0,0 +1,174 @@
+require_relative 'node'
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    new_tail = Node.new(value)
+
+    if @head.nil?
+      @head = new_tail
+    else
+      current_node = @head
+      current_node = current_node.next_node until current_node.next_node.nil?
+
+      current_node.next_node = new_tail
+    end
+  end
+
+  def prepend(value)
+    new_head = Node.new(value)
+
+    new_head.next_node = @head
+    @head = new_head
+  end
+
+  def size
+    current_node = @head
+    nodes = 0
+
+    until current_node.nil?
+      nodes += 1
+      current_node = current_node.next_node
+    end
+
+    return nodes
+  end
+
+  def head
+    return @head
+  end
+
+  def tail
+    return nil if @head.nil?
+
+    current_node = @head
+    current_node = current_node.next_node until current_node.next_node.nil?
+
+    return current_node
+  end
+
+  def at(index)
+    return nil if @head.nil?
+    return nil if index < 0
+  
+    current_node = @head
+    current_index = 0
+  
+    until current_index >= index
+      return nil if current_node.next_node.nil?
+
+      current_node = current_node.next_node
+      current_index += 1
+    end
+  
+    return current_node
+  end
+  
+  def pop
+    return nil if @head.nil?
+
+    if @head.next_node.nil?
+      popped_node = @head
+      @head = nil
+
+      return popped_node
+    end
+    
+    current_node = @head
+    current_node = current_node.next_node until current_node.next_node.next_node.nil?
+    
+    popped_node = current_node.next_node
+    current_node.next_node = nil
+    
+    return popped_node
+  end
+
+  def contains?(value)
+    return false if @head.nil?
+
+    current_node = @head
+    
+    until current_node.nil?
+      return true if current_node.value == value
+      current_node = current_node.next_node
+    end
+    
+    return false
+  end
+
+  def find(value)
+    return nil if @head.nil?
+
+    current_node = @head
+    index = 0
+
+    until current_node.nil?
+      return index if current_node.value == value
+      
+      current_node = current_node.next_node
+      index += 1
+    end
+
+    return nil
+  end
+
+  def to_s
+    string_nodes = ""
+    current_node = @head
+  
+    until current_node.nil?
+      string_nodes << "(#{current_node.value}) -> "
+      current_node = current_node.next_node
+    end
+  
+    return string_nodes << "nil"
+  end
+
+  def insert_at(value, index)
+    return nil if index < 0
+
+    added_node = Node.new(value)
+    
+    if index == 0
+      added_node.next_node = @head
+      @head = added_node
+    else
+      return nil if @head.nil?
+
+      current_node = @head
+    
+      (index - 1).times do
+        return nil if current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+    
+      added_node.next_node = current_node.next_node
+      current_node.next_node = added_node
+    end
+  end
+
+  def remove_at(index)
+    return nil if @head.nil? || index < 0
+  
+    if index == 0
+      removed_node = @head
+      @head = @head.next_node
+      return removed_node
+    end
+  
+    current_node = @head
+    (index - 1).times do
+      return nil if current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+  
+    removed_node = current_node.next_node
+    return nil if removed_node.nil?
+  
+    current_node.next_node = removed_node.next_node
+    return removed_node
+  end
+end
